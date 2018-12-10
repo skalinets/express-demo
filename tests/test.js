5c688ae14891f048765a9b700c686cc16549e37f
Serhiy Kalinets <kalinets@gmail.com>
2018-12-11 01:01:45 +0200

    fixing tests

--- tests/test.js
+++
@@ -1,9 +0,0 @@
-const request = require('supertest')
-
-var app = require('../index')
-
-describe('GET /', () => {
-  it('respond with hello world', done => {
-    request(app).get('/').expect('hello from js!', done)
-  })
-})
